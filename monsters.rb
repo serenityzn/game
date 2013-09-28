819c73074274ec23f28076268cc2dd45972e4260
nefer <nefer@nefer-System-Product-Name.(none)>
2013-09-28 14:55:23 +0300

    mushroom added

--- monsters.rb
+++ monsters.rb
@@ -164,3 +164,117 @@ class Turtle
 end
 
 
+class Mushroom
+	def initialize(window)
+	 @image = Gosu::Image.new(window, "media/mushroom.png", false)
+	 @mimgs = Array.new
+	 @mimgs.push(Gosu::Image.new(window, "media/mushroom.png", false))
+	 @mimgs.push(Gosu::Image.new(window, "media/mushroom1.png", false))
+	 @x = @y = @vel_x  = @angle = 0.0
+	 @route=0
+	 @anim=0
+	end
+
+	def warp(x,y)
+	 @x, @y = x, y
+	end
+
+	def turn_left
+	 x=@x-0.5
+	 if check(x,@y)==1
+	  @x-=0.5
+	 else
+	  @route=1
+	 end
+	end
+
+	def turn_right
+	 x=@x+0.5
+	 if check(x,@y)==1
+	  @x+=0.5
+	 else
+	  @route=0
+	 end
+	end
+	
+	def gravity
+	 y=@y+2.5
+	 if check(@x,y)==1
+	  @y+=2.5
+	 end
+	end	
+
+	def move
+		if @anim>-1 and @anim<11
+		 @image=@mimgs[0]
+		else
+		 @image=@mimgs[1]
+		end
+		@anim+=1
+
+		if @anim==20
+		 @anim=0
+		end
+	
+		if @route==1
+		 turn_right
+		else
+		 turn_left
+		end
+		
+	end
+
+	def draw
+	 x=@x/640
+	 xx=@x-x.to_i*640
+	 @image.draw_rot(xx, @y, 1, @angle)
+	end
+
+	def kill
+	 kill=0
+
+	 x=@x-$globx
+	 if x<0
+	  x*=-1
+	 end
+
+	 y=@y-$globy
+	 if y<0
+	  y*=-1
+	 end
+	 	 
+	 if x<30 and y<20
+	  kill=1
+	 end
+	 return kill
+	end
+	
+	def check(x,y)
+	 arsize = $lvl_xy[0].size
+	 a=$lvl_xy
+	 dr=1
+	i=0
+	while i<arsize
+         if (x > a[0][i].to_i-15 and x < a[0][i].to_i+55) and ( y+1> a[1][i].to_i-15 and y+1< a[1][i].to_i+55)
+                dr=0
+         end
+        i += 1
+	end
+	
+	return dr 
+	end
+
+	def cross
+	  a=@x/640
+	  b=$globx/640
+	   if a.to_i == b.to_i
+		result=1
+	   else
+		result=0
+	   end
+	 return result
+	end
+
+end
+
+
